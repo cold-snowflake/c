87cd6abd8525f1ed89bd165e93cd9aa6566d504a
cold-snowflake <cold.snowflake01@gmail.com>
2024-02-18 14:11:20 +0100

    programs that count spase, tab, and of str

---
+++ begin/get_put_char.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <unistd.h>
+
+// C program to count spaces in a file
+void count_space() 
+{
+    int c, nl;
+    nl = 0;
+    while ((c = getchar()) != EOF)
+        if(c == ' ')
+            ++nl;
+    if (nl == 0)
+        printf("This file has zero spases\n");
+    else if (nl == 1)
+        printf("This file contans only %d spase\n", nl);
+    else if (nl > 1)
+        printf("This file contans %d spases\n", nl);
+}
+
+// C program to count tab in a file
+void count_tab() 
+{
+    int c, nl;
+    nl = 0;
+    while ((c = getchar()) != EOF)
+        if(c == '\t')
+            ++nl;
+    if (nl == 0)
+        printf("This file has zero tabs\n");
+    else if (nl == 1)
+        printf("This file contans only %d tab\n", nl);
+    else if (nl > 1)
+        printf("This file contans %d tabs\n", nl);
+}
+
+// C program to count end of str in a file
+void count_end_str() 
+{
+    int c, nl;
+    nl = 0;
+    while ((c = getchar()) != EOF)
+        if(c == '\n')
+            ++nl;
+    if (nl == 0)
+        printf("This file has zero ends of str\n");
+    else if (nl == 1)
+        printf("This file contans only %d end of str\n", nl);
+    else if (nl > 1)
+        printf("This file contans %d ends of str\n", nl);
+}
+
+int main()
+{
+    count_space();
+    count_tab();
+    count_end_str();
+}
