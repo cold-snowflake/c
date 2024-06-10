3af16c584c49009cefb45779ff4e61fb3e96a1e8
cold-snowflake <cold.snowflake01@gmail.com>
2024-06-10 12:40:33 +0200

    assigment

---
+++ data_type/assignment_operations.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int bitcount (unsigned x)
+{
+    int b;
+
+    for (b = 0; x != 0; x >>= 1)
+    if (x & 01)
+    b++;
+    return b;
+}
+
+int main()
+{
+    int a = 5;
+    printf("%d\n", bitcount(a));
+    return 0;
+}
